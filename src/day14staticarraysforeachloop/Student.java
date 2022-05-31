21b85ca32edeec86e72dcdc5fc97614cfc9e7965
Sadrettin Akkoyun <sadrettin.akkoyun10@gmail.com>
2022-05-31 11:15:23 -0400

    Java commit

---
+++ src/day14staticarraysforeachloop/Student.java
@@ -0,0 +1,19 @@
+package day14staticarraysforeachloop;
+
+public class Student {
+
+    public static void main(String[] args) {
+
+    }
+
+    String name;
+    int year = 2022;
+    static int counter = 1000;
+    String studentId;
+    public String setStudentId(){
+        counter++;
+        studentId = year + "" + counter;
+        return studentId;//If the return type of the method is different from "void" you have to use "return" keyword inside the method
+    }
+
+}
