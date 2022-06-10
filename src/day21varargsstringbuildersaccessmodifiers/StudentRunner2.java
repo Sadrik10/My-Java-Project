e23ccc0d064e34f582e61ec3afa198264776865a
Sadrettin Akkoyun <sadrettin.akkoyun10@gmail.com>
2022-06-10 16:19:32 -0400

    Java commit

---
+++ src/day21varargsstringbuildersaccessmodifiers/StudentRunner2.java
@@ -0,0 +1,12 @@
+package day21varargsstringbuildersaccessmodifiers;
+
+public class StudentRunner2 {
+
+    public static void main(String[] args) {
+
+        Student std2 = new Student();
+
+
+    }
+
+}
