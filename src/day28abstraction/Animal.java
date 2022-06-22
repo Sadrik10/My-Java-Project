05da7514954fd932d19bdc1046636da33539da44
Sadrettin Akkoyun <sadrettin.akkoyun10@gmail.com>
2022-06-22 13:10:39 -0400

    Java Commit

---
+++ src/day28abstraction/Animal.java
@@ -0,0 +1,19 @@
+package day28abstraction;
+
+public abstract class Animal {
+
+          /*
+           Create animalSound abstract method
+           Create animalName abstract method
+          */
+
+    public abstract void animalSound();
+    public abstract void animalName();
+
+       /*
+         Create 2 concrete classes
+       1 will be Sheep
+       2 will be Cat
+         Then inherit Animal class
+       */
+}
