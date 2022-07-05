de318735fcdafac58c2b802a7202269d9f392542
Sadrettin Akkoyun <sadrettin.akkoyun10@gmail.com>
2022-07-05 11:11:17 -0400

    Java Commit

---
+++ src/practice01/Q03.java
@@ -0,0 +1,28 @@
+package practice01;
+
+import java.util.Scanner;
+
+public class Q03 {
+
+    public static void main(String[] args) {
+
+    /*
+      Type java code by using ternary and if-else. Ask user to enter two integers
+      Write a program to print the maximum  one on the console.(Do not forget they may be equal)
+    */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter two integers..");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println( a == b     ?   "They are equal "   :    a > b ?    a  :  b );
+
+
+
+
+
+
+
+    }
+}
