8bd9a310981f3b9dc9e5edd12085d6e24ae9ba3d
Sadrettin Akkoyun <sadrettin.akkoyun10@gmail.com>
2022-07-28 23:17:51 -0400

    Java commit

---
+++ src/practice05/Q02.java
@@ -0,0 +1,25 @@
+package practice05;
+
+public class Q02 {
+
+    public static void main(String[] args) {
+
+        /*
+        String s1 = "java";                             // *** String ***
+        StringBuilder s2 = new StringBuilder("java");  // *** StringBuilder ***
+        if(s1 == s2){
+            System.out.println("1");
+        }
+        if(s1.equals(s2)){
+            System.out.println("2");
+        }
+        A. 1
+        B. 2
+        C. 12
+        D. No output is printed
+        E. Gives error **** Correct answer **** // String and StringBuilder are different data types.
+         */
+    }
+}
+
+
